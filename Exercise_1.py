ad698956e506f7b84e065be688ab61a2be33e9a8
MrGunsoul <nico.kranni@hotmail.com>
2022-01-18 20:20:38 +0200

    Changed prints
    Changed prints to fit better desired output.

--- Exercise_1.py
+++ Exercise_1.py
@@ -11,16 +11,18 @@ def numlist(): #Start of function
     while len(emptynum)<10:#Min 10 items
         emptynum.append(int(input()))
     else:
-        print(emptynum.sort)#After then items prints list of inputs. And we also sort them for task 3
+        emptynum.sort()
+        print("Arranged list: ", *emptynum, sep=",")#After then items prints list of inputs. And we also sort them for task 3
     emptynum=[(random.randrange(0,999))for item in emptynum] #Replaces every element in list with random one between 0 and 999
-    print(emptynum.sort) #Sort the list for task 3
+    emptynum.sort()
+    print(*emptynum, sep=",") #Sort the list for task 3
 
 def strlist(): #Start of function
     emptystr=[] #Empty list
     while len(emptystr)<10:#Min 10 items
         emptystr.append(str(input()))
     else:
-        print(emptystr)#After then items prints list of inputs
+        print("Strings: ", *emptystr,sep=",")#After then items prints list of inputs
 
 
 #numlist()
@@ -33,18 +35,20 @@ def readint(): #Start of function
     listsum=[]
     while True:
         try:
-            number=int(input("Give a number: "))
+            number=int(input("Please give an integer: "))
         except ValueError:
             print ("Has to be a number")
             continue
         if number == 0:
-            print(negint)
-            print(evenint)
+            print("Number of negative integers is: ", negint)
+            print("Number of even integers is: ", evenint)
             Sum=sum(listsum)
-            print(Sum)
+            print("Sum of positive integers divisible by three is: ", Sum)
             break
         if number < 0:
             negint+=1
+            if number % 2==0:
+                evenint+=1
             continue
         if number % 2==0:
             evenint+=1
@@ -55,23 +59,29 @@ def readint(): #Start of function
             listsum.append(number)
             continue
 
+
+
 #Task 7
 def ariprog():
     terms=0
     termsl=[]
     sumofterms=[]
     sqrofterms=[]
-    x=int(input("Give a number: "))
+    x=int(input("Give maximum value: "))
     numbers=range(3, x, 3)
     for i in numbers:
         terms+=1
         termsl.append(i)
         sumofterms.append(i)
         sqrofterms.append((i*i))
-    print(termsl)
-    print (terms)
-    print(sum(sumofterms))
-    print(sum(sqrofterms))
+    print("Procession is: ",*termsl)
+    print ("Number of terms is: ", terms)
+    print("Sumf of terms is: ",sum(sumofterms))
+    print("Sum of squared terms is: ", sum(sqrofterms))
+
+ariprog()
+
+
 
 #Task 8
 def rps():
@@ -132,3 +142,13 @@ def rps():
         print ("You lost!")
     else:
         print("You won!")
+
+
+#Task 9
+
+def ranint():
+    return random.randrange(1,7)
+    
+
+
+#print (ranint())
